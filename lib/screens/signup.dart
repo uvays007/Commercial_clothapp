5e88878d67317b2240e2b62b8fe47be03c35eea2
uvays007 <uviuvais1@gmail.com>
2025-10-10 20:34:09 +0530

    sign up

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -26,6 +26,43 @@ class Signup extends StatelessWidget {
                 color: Color(0xFFC19375),
               ),
             ),
+            SizedBox(height: 50),
+            Text(
+              'Create an account',
+              style: TextStyle(
+                fontSize: 16,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Text(
+              'Enter your email to sign up for this app',
+              style: TextStyle(
+                fontSize: 16,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            SizedBox(height: 15),
+            Padding(
+              padding: EdgeInsetsGeometry.all(25),
+              child: Container(
+                height: 40,
+                child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.fromLTRB(15, 10, 10, 10),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(color: Color(0xFF828282)),
+                    ),
+                    hintText: 'Enter your Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
