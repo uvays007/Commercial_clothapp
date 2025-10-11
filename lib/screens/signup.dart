27c64514d0f9fdc9489b53a7d18dfa857d565a62
uvays007 <uviuvais1@gmail.com>
2025-10-11 16:54:32 +0530

    signup completed

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Signup extends StatelessWidget {
   const Signup({super.key});
@@ -43,24 +44,129 @@ class Signup extends StatelessWidget {
                 fontWeight: FontWeight.w400,
               ),
             ),
-            SizedBox(height: 15),
+            SizedBox(height: 40),
             Padding(
-              padding: EdgeInsetsGeometry.all(25),
-              child: Container(
-                height: 40,
-                child: TextField(
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.fromLTRB(15, 10, 10, 10),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                      borderSide: BorderSide(color: Color(0xFF828282)),
-                    ),
-                    hintText: 'Enter your Email',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
+              padding: EdgeInsetsGeometry.only(left: 20, right: 20),
+              child: Column(
+                children: [
+                  Container(
+                    height: 40,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.fromLTRB(15, 10, 10, 10),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: Color(0xFF828282)),
+                        ),
+                        hintText: 'Enter your Email',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                  SizedBox(height: 12),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        foregroundColor: Colors.white,
+                        backgroundColor: Color(0xFFC19375),
+                      ),
+                      onPressed: () {},
+                      child: Text('Continue'),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          color: Color(0xFFE6E6E6),
+                          thickness: 1,
+                          endIndent: 10,
+                        ),
+                      ),
+                      Text(
+                        'or',
+                        style: TextStyle(
+                          color: Color(0xFF828282),
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          color: Color(0xFFE6E6E6),
+                          thickness: 1,
+                          indent: 10,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        foregroundColor: Colors.black,
+                        backgroundColor: Color(0xFFEEEEEE),
+                      ),
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          SvgPicture.asset(
+                            height: 20,
+                            width: 20,
+                            'assets/icons/Google_Favicon_2025.svg',
+                          ),
+                          SizedBox(width: 5),
+
+                          Text('Continue with Google'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        foregroundColor: Colors.black,
+                        backgroundColor: Color(0xFFEEEEEE),
+                      ),
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          SvgPicture.asset(
+                            height: 20,
+                            width: 20,
+                            'assets/icons/Apple_logo_black.svg',
+                          ),
+                          SizedBox(width: 5),
+
+                          Text('Continue with Apple'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'By clicking continue, you agree to our Terms of Service and Privacy Policy',
+                    style: TextStyle(
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
